90c1459a5284c4030ca8e7938eb55df31f6b256d
VladimirBabin <vladimir.sol.rojo@gmail.com>
2024-03-12 00:12:19 +0200

    Added necessary properties and annotations to entity classes.

--- emo-check/src/main/java/com/github/vbabin/emocheck/state/StateLog.java
+++ emo-check/src/main/java/com/github/vbabin/emocheck/state/StateLog.java
@@ -1,17 +1,24 @@
 package com.github.vbabin.emocheck.state;
 
 import com.github.vbabin.emocheck.user.User;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class StateLog {
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
     private User user;
     private State state;
     private LocalDateTime dateTime;
--- emo-check/src/main/java/com/github/vbabin/emocheck/state/StateLogRepository.java
+++ emo-check/src/main/java/com/github/vbabin/emocheck/state/StateLogRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Repository
 public interface StateLogRepository extends CrudRepository<StateLog, Long> {
     List<StateLog> findAllByUserAndDateTimeAfter(User user, LocalDateTime dateTime);
 }
