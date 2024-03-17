ba0d9121f35a4c07affeb631e1136c50b7760560
VladimirBabin <vladimir.sol.rojo@gmail.com>
2024-03-17 14:43:44 +0200

    Changed css to fix bugs on iOS.

--- emo-check-frontend/src/App.js
+++ emo-check-frontend/src/App.js
@@ -12,7 +12,9 @@ function App() {
                     <h3>Welcome to emotions tracker</h3>
                     <h6>To log your state tap the button:</h6>
                 </div>
-                <button className="new-log" onClick={() => setButtonPopup(true)}>+</button>
+                <div className="plus-button-div">
+                    <button className="new-log" onClick={() => setButtonPopup(true)}>+</button>
+                </div>
             </main>
             <div className="popup-div">
                 <Popup trigger={buttonPopup} setTrigger={setButtonPopup}/>
